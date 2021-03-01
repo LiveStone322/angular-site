0a526e6824dbbecfa3c62ea1763f71ec1e6e4276
Fedulov Anton <live.stone.anton@gmail.com>
2021-03-01 08:42:58 +0500

    v0.981

--- scully.my-site.config.ts
+++ scully.my-site.config.ts
@@ -2,7 +2,7 @@ import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'my-site',
-  outDir: './static/dist',
+  outDir: './dist/static',
   routes: {},
   handle404: 'index',
 };
